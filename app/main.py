df924b2d7a7162c847d6e222a33b8eae88fda8c8
luuna078050 <luuna078050@gmail.com>
2026-08-24 07:25:54 +0200

    Enable final Fast Scalper mobile controls

--- app/main.py
+++ app/main.py
@@ -19,10 +19,12 @@ try:
     from .report_v6_patch import install as _install_report_v6
     from .account_balance_patch import install as _install_account_balance
     from .report_v7_patch import install as _install_report_v7
+    from .ui_controls_patch import install as _install_ui_controls
     _install_paper_mode(app)
     _install_allocation_fix(app)
     _install_report_v6()
     _install_account_balance()
     _install_report_v7()
+    _install_ui_controls(app)
 finally:
     threading.Thread.start = _original_thread_start
