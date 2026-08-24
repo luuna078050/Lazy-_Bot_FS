0aa4b6db2f5d28c0327a37dc5020666002621287
luuna078050 <luuna078050@gmail.com>
2026-08-24 08:27:26 +0200

    Apply final targeted Fast Scalper UI patch

--- app/main.py
+++ app/main.py
@@ -20,11 +20,13 @@ try:
     from .account_balance_patch import install as _install_account_balance
     from .report_v7_patch import install as _install_report_v7
     from .ui_controls_patch import install as _install_ui_controls
+    from .final_ui_patch import install as _install_final_ui
     _install_paper_mode(app)
     _install_allocation_fix(app)
     _install_report_v6()
     _install_account_balance()
     _install_report_v7()
     _install_ui_controls(app)
+    _install_final_ui(app)
 finally:
     threading.Thread.start = _original_thread_start
