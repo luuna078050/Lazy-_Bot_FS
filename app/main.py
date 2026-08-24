5b9e044053212a0286ab2b0560047b0abb500a77
luuna078050 <luuna078050@gmail.com>
2026-08-24 21:53:20 +0200

    Activate PAPER v3 engine and robust top-pair picker

--- app/main.py
+++ app/main.py
@@ -20,11 +20,15 @@ try:
     from .account_balance_patch import install as _install_account_balance
     from .report_v7_patch import install as _install_report_v7
     from .ui_controls_patch import install as _install_ui_controls
+    from .paper_api_v3_patch import install as _install_paper_api_v3
+    from .ui_pair_fix_patch import install as _install_ui_pair_fix
     _install_paper_mode(app)
     _install_allocation_fix(app)
     _install_report_v6()
     _install_account_balance()
     _install_report_v7()
     _install_ui_controls(app)
+    _install_paper_api_v3(app)
+    _install_ui_pair_fix(app)
 finally:
     threading.Thread.start = _original_thread_start
