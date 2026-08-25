cc48f2e5cef3d8c5cf4f9554505251570c5df904
luuna078050 <luuna078050@gmail.com>
2026-08-25 20:00:26 +0200

    Fast Scalper: activate persistent PAPER session resume

--- app/main.py
+++ app/main.py
@@ -18,6 +18,7 @@ from .final_ui_v13_patch import install as _install_final_ui_v13
 from .reinvest_ui_patch import install as _install_reinvest_ui
 from .reinvest_engine_patch import install as _install_reinvest_engine
 from .recent_ui_patch import install as _install_recent_ui
+from .paper_persistence_patch import install as _install_paper_persistence
 from .final_fix_v14_patch import install as _install_final_fix_v14
 
 _install_paper_mode(app)
@@ -31,6 +32,7 @@ _install_paper_api_v3(app)
 _install_ui_pair_fix(app)
 _install_multi_slot(app)
 _install_reinvest_engine()
+_install_paper_persistence()
 _install_final_ui(app)
 _install_final_ui_v13(app)
 _install_reinvest_ui(app)
