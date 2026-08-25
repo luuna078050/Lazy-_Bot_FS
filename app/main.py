9aa9e80e78a00c6218dee5fe153461df2bab1891
luuna078050 <luuna078050@gmail.com>
2026-08-25 16:21:29 +0200

    Fast Scalper: wire final trading and UI fixes

--- app/main.py
+++ app/main.py
@@ -10,8 +10,10 @@ from .report_v6_patch import install as _install_report_v6
 from .account_balance_patch import install as _install_account_balance
 from .report_v7_patch import install as _install_report_v7
 from .ui_controls_patch import install as _install_ui_controls
+from .profit_gate_patch import install as _install_profit_gate
 from .paper_api_v3_patch import install as _install_paper_api_v3
 from .ui_pair_fix_patch import install as _install_ui_pair_fix
+from .final_ui_patch import install as _install_final_ui
 
 _install_paper_mode(app)
 _install_allocation_fix(app)
@@ -19,5 +21,7 @@ _install_report_v6()
 _install_account_balance()
 _install_report_v7()
 _install_ui_controls(app)
+_install_profit_gate()
 _install_paper_api_v3(app)
 _install_ui_pair_fix(app)
+_install_final_ui(app)
