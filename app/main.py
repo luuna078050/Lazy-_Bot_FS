c1b2fe1fc5ba11c3dd9bd28cb58a41461fd63900
luuna078050 <luuna078050@gmail.com>
2026-08-26 16:57:11 +0200

    Test: attach repaired bot start controls

--- app/main.py
+++ app/main.py
@@ -18,6 +18,7 @@ from .final_ui_v13_patch import install as _install_final_ui_v13
 from .reinvest_ui_patch import install as _install_reinvest_ui
 from .reinvest_engine_patch import install as _install_reinvest_engine
 from .recent_ui_patch import install as _install_recent_ui
+from .test_start_repair_patch import install as _install_test_start_repair
 
 _install_paper_mode(app)
 _install_allocation_fix(app)
@@ -34,3 +35,4 @@ _install_final_ui(app)
 _install_final_ui_v13(app)
 _install_reinvest_ui(app)
 _install_recent_ui(app)
+_install_test_start_repair(app)
